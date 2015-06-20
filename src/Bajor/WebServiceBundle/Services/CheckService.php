7d7f9cbb7ee74603a709b2372c06269efd324d37
ppiatek <przemek.piatek@gmail.com>
2015-06-20 16:36:33 +0200

    New 02

---
+++ src/Bajor/WebServiceBundle/Services/CheckService.php
@@ -0,0 +1,18 @@
+<?php 
+# src/Bajor/WebServiceBundle/Services/CheckService.php
+
+namespace Bajor\WebServiceBundle\Services;
+
+class CheckService
+{
+    /**
+     * Check soap service, display name when called
+     * @param string $name 
+     * @return mixed
+     */
+    public function check($name)
+    {
+        
+        return 'Hello '.$name;
+    }
+}
